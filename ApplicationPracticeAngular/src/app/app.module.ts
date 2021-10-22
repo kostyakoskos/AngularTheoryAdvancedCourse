e9f30098418c635cbccb0403620348cec2d9e2cc
kostyakoskos <kanstantsin.karatkevich@arcadia.spb.ru>
2021-10-22 16:56:42 +0300

    Add Forms, validation try1

--- ApplicationPracticeAngular/src/app/app.module.ts
+++ ApplicationPracticeAngular/src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
-import { ComponentsComponent } from './admin/shared/components/components.component';
+import { PostComponent } from './shared/components/post/post.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,7 @@ import { ComponentsComponent } from './admin/shared/components/components.compon
     MainLayoutComponent,
     HomePageComponent,
     PostPageComponent,
-    ComponentsComponent
+    PostComponent
   ],
   imports: [
     BrowserModule,
